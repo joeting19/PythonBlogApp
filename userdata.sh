b955c3a05516c6f03bf1062d343a0716e6b0bb7f
joe <joeting919@gmail.com>
2023-03-16 22:06:51 -0600

    add Dockerfile

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #shell script to prepare for ubuntu 20.04 to host webapp
 
-sudo apt update
-sudo apt install git
+sudo apt update -y
+sudo apt install git -y
 git clone https://github.com/joeting19/PythonBlogApp.git
 cd PythonBlogApp
 sudo apt install python3.8-venv -y
@@ -10,4 +10,4 @@ source venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 main.py
 
-#tmux, start processm ctrl+b and then d to exit
+#tmux, start processm ctrl+b and then d to exitpy
