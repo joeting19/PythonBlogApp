a9c8344103a6fac788a1c18aedb91daf6745c6aa
joe <joeting919@gmail.com>
2023-03-21 20:41:34 -0600

    test

--- shellscript.sh
+++ shellscript.sh
@@ -7,10 +7,4 @@ docker run -d -p 80:80 $IMAGE_NAME
 
 
 
-IMAGE_NAME='joeting91/pythonblog:latest' \
-CONTAINER=$(docker ps | grep $IMAGE_NAME | awk '{print $1;}') \
-docker kill $CONTAINER \
-docker pull $IMAGE_NAME \
-docker run -d -p 80:80 $IMAGE_NAME
-
 
