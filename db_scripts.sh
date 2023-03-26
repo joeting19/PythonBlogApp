cdfb86dae959019331b75d9b920158c6cec6f723
joe <joeting919@gmail.com>
2023-03-26 15:36:45 -0600

    ignore db scripts

--- db_scripts.sh
+++ db_scripts.sh
@@ -1,16 +1,19 @@
-#create db container and connect
+#this file contains scripts for db admin tasks...
+#creating and persisting data for the blog app using postgres container
+#create a docker volume to persist data...
 
-docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+#create db container and connect
 
 docker volume create dbvol
 
-docker exec -it 52a607adb832 psql -U postgres -d postgres
+sudo apt install azure-cli
+
+docker run --name postgres -e POSTGRES_PASSWORD=postgres -v dbvol:/var/lib/postgresql/data -p 5432:5432 -d postgres
+
+docker exec -it 98ba2466d449 psql -U postgres -d postgres
 
 
-
-docker exec -it 52a607adb832 env | grep POSTGRES_URI
-
-
+#if running container table for first time, create this table
     CREATE TABLE blog_post (
             id SERIAL PRIMARY KEY, 
             title VARCHAR (250) NOT NULL, date VARCHAR (250) NOT NULL, 
@@ -19,3 +22,64 @@ docker exec -it 52a607adb832 env | grep POSTGRES_URI
             UNIQUE (title)
             );
 
+#create snapshot
+
+#think about whetehr to just 
+
+#trigger azure function to create snapshot...!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+#first kill container
+docker kill $(containder_id)
+
+#now run az snapshot... have to login first.
+az snapshot create --resource-group blogapp --name blogsnapshot --source postgres_OsDisk_1_e0f0f2447c7e453c9a2a8079d6086dc8
+
+#start up db again
+docker run --name postgres -e POSTGRES_PASSWORD=postgres -v dbvol:/var/lib/postgresql/data -p 5432:5432 -d postgres
+
+
+#troubleshooting
+
+docker exec -it c30e025381da env | grep POSTGRES_URI
+
+
+#install azcopy
+
+
+
+#copy to storage using azcopy
+
+
+dbvol
+
+/var/lib/docker/volumes/dbvol
+
+
+#tried using az storage copy... ended up crashing the vm. Had to shut down the container to be avble to run it. so this wasnt ideal.
+az storage file copy
+
+#use az file to copy the postgres data...
+azcopy copy '/var/lib/docker/volumes/dbvol' 'https://blogappimages.file.core.windows.net/postgresbackup/?sv=2021-12-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-03-27T02:16:18Z&st=2023-03-26T18:16:18Z&spr=https&sig=GeCuWvJ2hfREnNb6hmdBNFatiGlJQT0cd04BhaJaAN0%3D' --recursive=true
+
+
+#uploading to blob doesnt work since it is a folder...
+azcopy cp /var/lib/docker/volumes/dbvol "https://blogappimages.blob.core.windows.net/postgresbackup/?sv=2021-12-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-03-27T02:16:18Z&st=2023-03-26T18:16:18Z&spr=https&sig=GeCuWvJ2hfREnNb6hmdBNFatiGlJQT0cd04BhaJaAN0%3D" --recursive=true
+
+
+docker run \
+  --net=host \
+  -e DATA_SOURCE_NAME="postgresql://postgres:postgres@localhost:5432/postgres?sslmode=disable" \
+  quay.io/prometheuscommunity/postgres-exporter
+
+
+
+
+
+
+
+#snapshot with az cli
+az snapshot create --resource-group blogapp --name blogsnapshot --source postgres_OsDisk_1_e0f0f2447c7e453c9a2a8079d6086dc8
+
+
+az snapshot create --resource-group blogapp --name blogsnapshot --source postgres_OsDisk_1_e0f0f2447c7e453c9a2a8079d6086dc8
