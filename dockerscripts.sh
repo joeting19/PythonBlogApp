4f897f2b801e68dc52327cf3fa702d88ce012da4
joe <joeting919@gmail.com>
2023-03-28 23:20:22 -0600

    test

--- dockerscripts.sh
+++ dockerscripts.sh
@@ -1,3 +1,7 @@
+ sudo systemctl start docker
+
+ sudo systemctl start docker
+sudo chmod 666 /var/run/docker.sock
  docker pull joeting91/pythonblog:dev
 
  docker run -d -p 80:80 joeting91/pythonblog:dev
