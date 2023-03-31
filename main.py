92c2e1a75de90568eb92ff0671b23715962714cd
joe <joeting919@gmail.com>
2023-03-30 21:43:17 -0600

    adding new page

--- main.py
+++ main.py
@@ -136,6 +136,10 @@ def delete_post(post_id):
 def about():
     return render_template("about.html")
 
+@app.route("/spirit")
+def about():
+    return render_template("spirit.html")
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
